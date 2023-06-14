a40b68f932c15cd5a4be40dac91f01f7c8895a8a
Fabian <mail@fabianhutter.com>
2023-06-14 11:32:37 +0200

    Updated version of the __parse_prerender_script method to handle IndexError when attempting to access specific indices of the prerender script.

--- googlelens/googlelens.py
+++ googlelens/googlelens.py
@@ -13,59 +13,49 @@ class GoogleLens:
             {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0'}
         )
 
-    def __get_prerender_script(self, page: str):
-        soup = BeautifulSoup(page, 'html.parser')
-
-        prerender_script = list(filter(
-            lambda s: (
-                'AF_initDataCallback(' in s.text and
-                re.search(r"key: 'ds:(\d+)'", s.text).group(1) == "0"),
-            soup.find_all('script')))[0].text
-        prerender_script = prerender_script.replace(
-            "AF_initDataCallback(", "").replace(");", "")
-        hash = re.search(r"hash: '(\d+)'", prerender_script).group(1)
-        prerender_script = prerender_script.replace(
-            f"key: 'ds:0', hash: '{hash}', data:",
-            f"\"key\": \"ds:0\", \"hash\": \"{hash}\", \"data\":").replace("sideChannel:", "\"sideChannel\":")
-
-        prerender_script = json.loads(prerender_script)
-
-        return prerender_script['data'][1]
-
     def __parse_prerender_script(self, prerender_script):
-        data = {
-            "match": None,
-            "similar": []
-        }
+    data = {
+        "match": None,
+        "similar": []
+    }
 
-        try:
+    try:
+        if prerender_script and prerender_script[0]:
             data["match"] = {
                 "title": prerender_script[0][1][8][12][0][0][0],
                 "thumbnail": prerender_script[0][1][8][12][0][2][0][0],
                 "pageURL": prerender_script[0][1][8][12][0][2][0][4]
             }
-        except IndexError:
-            pass
-        
-        if data["match"] is not None:
+    except (IndexError, KeyError):
+        pass
+
+    if data["match"] is not None:
+        try:
             visual_matches = prerender_script[1][1][8][8][0][12]
-        else:
-            try:
-                visual_matches = prerender_script[0][1][8][8][0][12]
-            except IndexError:
-                return data
+        except (IndexError, KeyError):
+            return data
+    else:
+        try:
+            visual_matches = prerender_script[0][1][8][8][0][12]
+        except (IndexError, KeyError):
+            return data
 
-        for match in visual_matches:
-            data["similar"].append(
-                {
-                    "title": match[3],
-                    "thumbnail": match[0][0],
-                    "pageURL": match[5],
-                    "sourceWebsite": match[14]
-                }
-            )
+    for match in visual_matches:
+        try:
+            title = match[3]
+            thumbnail = match[0][0]
+            pageURL = match[5]
+            sourceWebsite = match[14]
+            data["similar"].append({
+                "title": title,
+                "thumbnail": thumbnail,
+                "pageURL": pageURL,
+                "sourceWebsite": sourceWebsite
+            })
+        except (IndexError, KeyError):
+            pass
 
-        return data
+    return data
 
     def search_by_file(self, file_path: str):
         multipart = {'encoded_image': (file_path, open(file_path, 'rb')), 'image_content': ''}
